b882736d1c244dbf7f35efcc861e38d6728eeb3d
Vishnu <vishnukdas@lambdatest.com>
2025-01-24 15:46:05 +0530

    updatedworkflowandyaml

--- sdk/playwrightLocal.js
+++ sdk/playwrightLocal.js
@@ -15,25 +15,25 @@ await page.setViewportSize({ width: 1280, height: 1024 });
         ignoreType: ['layout']
     };
 
-    // await page.goto("https://login.salesforce.com/");
+    await page.goto("https://kaneai.my.salesforce.com/");
 
 
-    // const username =process.env.username 
-    // const password=process.env.password
-    // // Locate and fill in the username field
-    // await page.fill("#username", username);
-    // // Locate and fill in the password field
-    // await page.fill("#password", password);
-    // // Locate and click the login button
-    // await page.click("#Login");
-    // // Wait for navigation to the dashboard (or a specific element to be loaded)
-    // await page.waitForURL("**/home", { timeout: 10000 });
-    // console.log("Login successful, navigated to dashboard");
-    // await page.waitForTimeout(20000);
+    const username =process.env.username 
+    const password=process.env.password
+    // Locate and fill in the username field
+    await page.fill("#username", username);
+    // Locate and fill in the password field
+    await page.fill("#password", password);
+    // Locate and click the login button
+    await page.click("#Login");
+    // Wait for navigation to the dashboard (or a specific element to be loaded)
+    await page.waitForURL("**/home", { timeout: 10000 });
+    console.log("Login successful, navigated to dashboard");
+    await page.waitForTimeout(20000);
 
-    // console.log("Login successful, waited for 20 seconds after login");
-    // await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
-    // await smartuiSnapshot.smartuiSnapshot(page, "salesForce", comparisonType);
+    console.log("Login successful, waited for 20 seconds after login");
+    await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
+    await smartuiSnapshot.smartuiSnapshot(page, "salesForce", comparisonType);
 
     await page.goto("https://www.atlassian.com/");
     await page.waitForTimeout(5000);
